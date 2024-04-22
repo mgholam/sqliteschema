2ab776a6ce7b61b93de95c882de00313fb12bae0
M. Gholam <mgholam@gmail.com>
2024-04-22 10:00:40 +0330

    - build script

---
+++ build.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+mkdir output
+
+ANAME="sqliteschema"
+ZIG="zig cc -static "
+# -------------------------------------------------------------
+echo "building linux native..."
+
+ON=${ANAME}
+time -p \
+GOOS=linux \
+CGO_ENABLED=1 \
+CC="${ZIG} -target native-native-musl" \
+go build -o output/${ON} -ldflags "-w -s" ./
+strip output/${ON}
+
+# -------------------------------------------------------------
+echo "building linux x64 musl..."
+
+ON=${ANAME}-linux
+time -p \
+GOOS=linux \
+GOARCH=amd64 \
+CGO_ENABLED=1 \
+CC="${ZIG} -target x86_64-linux-musl" \
+go build -o output/${ON} -ldflags "-w -s" ./
+strip output/${ON}
+
+
+# -------------------------------------------------------------
+# echo "building windows..."
+
+# ON=${ANAME}64.exe
+# CGO_ENABLED=1 \
+# GOOS=windows \
+# GOARCH=amd64 \
+# CC="${ZIG} -static -target x86_64-windows-gnu" \
+# go build -o output/${ON} -ldflags "-w -s" ./
+
+# ON=${ANAME}386.exe
+# CGO_ENABLED=1 \
+# GOOS=windows \
+# GOARCH=386 \
+# CC="${ZIG} -static -target i386-windows-gnu" \
+# go build -o output/${ON} -ldflags "-w -s" ./
+
+# -------------------------------------------------------------
+# echo "building android x64..."
+
+# ON=${ANAME}-android
+# GOOS=linux \
+# GOARCH=arm64 \
+# CGO_ENABLED=1 \
+# CC="${ZIG} -static -target aarch64-linux-musl" \
+# go build -o output/${ON} -ldflags "-w -s" ./
+# strip output/${ON}
+# -------------------------------------------------------------
+# echo "building pi zero w..."
+
+# ON=${ANAME}-pizw
+# GOARM=5 \
+# GOOS=linux \
+# GOARCH=arm \
+# CGO_ENABLED=1 \
+# CC="${ZIG} -static -target arm-linux-musleabihf -march=arm1176jzf_s" \
+# go build -o output/${ON} -ldflags "-w -s" ./
+# strip output/${ON}
+# -------------------------------------------------------------
+# echo "building darwin..."
+
+# CGO_ENABLED=1 \
+# GOOS=darwin \
+# GOARCH=arm64 \
+# CC="zig cc -target aarch64-macos-gnu" \
+# go build -o output/${ANAME}-darwin -ldflags "-w -s" ./
+
